4a1f7365fedd422792767e9559d09768e4e8024a
GABRIEL HENRIQUE CECCON <gabriel_ceccon@estudante.sesisenai.org.br>
2025-04-04 15:27:01 -0300

    Movimento da bola e raquete pronto apenas falta pontuação e bricks

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 const canvas = document.getElementById('jogo')
 const ctx = canvas.getContext('2d')
-// não usei getters and setter por questão de tempo :P
-document.addEventListener ('keydown', function(event) {
-    if (event.key === 'D') {
-        raquete1.x -= 10
-    } else if (event.key === 'A') {
-        raquete1.x += 10
+
+let velocidadeX = 0
+document.addEventListener('keydown', function(event) { 
+    if (event.key === 'a') {
+        velocidadeX = -10;  
+    } else if (event.key === 'd') {
+        velocidadeX = 10;   
     }
-})
+});
+
+document.addEventListener('keyup', function(event) { 
+    if (event.key === 'a' || event.key === 'd') {
+        velocidadeX = 0;  
+    }
+});
+
+
+
 class Entidade {
     constructor(x, y, largura, altura, cor) {
         this.x = x
@@ -25,24 +35,42 @@ class Entidade {
 class Bola extends Entidade {
     constructor(x, y, largura, altura, cor) {
         super(x, y, largura, altura, cor)
-        this.velocidadeX
-        this.velocidadeY
+        this.velocidadeX = 3
+        this.velocidadeY = 4
     }
     atualizar() {
         this.x += this.velocidadeX
         this.y += this.velocidadeY
     }
+    verificarColisao(entidade) {
+        
+         if (entidade.x < this.x + this.largura && 
+            entidade.x + entidade.largura > this.x &&
+            entidade.y < this.y + this.altura && 
+            entidade.y + entidade.altura > this.y || 0 >=  this.y  ) {
+            this.velocidadeY = -this.velocidadeY;
+        }
+        if (this.x < 0 || this.x + this.largura > canvas.width) {
+            this.velocidadeX = -this.velocidadeX;
+        }
+        console.log(canvas.width)
+        console.log(this.x)
+
+    }
+    
 }
-
-
-
 
 class Raquete extends Entidade {
     constructor(x, y, largura, altura, cor) {
         super(x, y, largura, altura, cor)
     }
-    ColisaoBola() {
-       
+    atualizar() {
+        this.x += velocidadeX
+        if (this.x < 0) {
+            this.x = 0
+        } else if (this.x + this.largura > canvas.width) {
+            this.x = canvas.width - this.largura
+        }
     }
 }
 
@@ -50,27 +78,30 @@ class Brick extends Entidade {
     constructor(x, y, largura, altura, cor) {
         super(x, y, largura, altura, cor)
     }
-    ColisaoBola() {
-
-    }
 }
 
 class Score extends Entidade {
     constructor(x, y, largura, altura, cor) {
         super(x, y, largura, altura, cor)
-        this.score
+        this.score = 0
     }
     desenhar() {
         ctx.fillStyle = this.cor
         ctx.font = "italic bold 35pt Tahoma";
-        fillText(score, this.x, this.y)
+        ctx.fillText(this.score, this.x, this.y)
     }
 }
 
+const bola = new Bola(300, 100, 10, 10, 'white')
+const raquete1 = new Raquete(500, 370, 100, 10, 'blue')
 
 function loop() {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
-    
+    bola.atualizar()
+    bola.verificarColisao(raquete1)
+    raquete1.atualizar()
+    bola.desenhar()
+    raquete1.desenhar()
     requestAnimationFrame(loop)
 }
 loop()
