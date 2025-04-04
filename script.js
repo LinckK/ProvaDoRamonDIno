89039bc4914c199850eff560f94b355f6079a829
GABRIEL HENRIQUE CECCON <gabriel_ceccon@estudante.sesisenai.org.br>
2025-04-04 14:49:00 -0300

    setup completo

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const canvas = document.getElementById('jogo')
 const ctx = canvas.getContext('2d')
-
+// não usei getters and setter por questão de tempo :P
+document.addEventListener ('keydown', function(event) {
+    if (event.key === 'D') {
+        raquete1.x -= 10
+    } else if (event.key === 'A') {
+        raquete1.x += 10
+    }
+})
 class Entidade {
     constructor(x, y, largura, altura, cor) {
         this.x = x
@@ -15,11 +22,55 @@ class Entidade {
     }
 }
 
-const objeto_na_tela = new Entidade(0, 0, 100, 100, 'red')
+class Bola extends Entidade {
+    constructor(x, y, largura, altura, cor) {
+        super(x, y, largura, altura, cor)
+        this.velocidadeX
+        this.velocidadeY
+    }
+    atualizar() {
+        this.x += this.velocidadeX
+        this.y += this.velocidadeY
+    }
+}
+
+
+
+
+class Raquete extends Entidade {
+    constructor(x, y, largura, altura, cor) {
+        super(x, y, largura, altura, cor)
+    }
+    ColisaoBola() {
+       
+    }
+}
+
+class Brick extends Entidade {
+    constructor(x, y, largura, altura, cor) {
+        super(x, y, largura, altura, cor)
+    }
+    ColisaoBola() {
+
+    }
+}
+
+class Score extends Entidade {
+    constructor(x, y, largura, altura, cor) {
+        super(x, y, largura, altura, cor)
+        this.score
+    }
+    desenhar() {
+        ctx.fillStyle = this.cor
+        ctx.font = "italic bold 35pt Tahoma";
+        fillText(score, this.x, this.y)
+    }
+}
+
 
 function loop() {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
-    objeto_na_tela.desenhar()
+    
     requestAnimationFrame(loop)
 }
 loop()
